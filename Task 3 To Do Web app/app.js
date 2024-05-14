570fa76d338f6f21fc3a34f6f5c658d60355adf1
Balla Kavya <ballakavya594@gmail.com>
2024-05-14 12:08:25 +0530

    To-Do List Update

--- Task 3 To Do Web app/app.js
+++ Task 3 To Do Web app/app.js
@@ -27,10 +27,14 @@ let display = () => {
   let finalTask = "";
   tasks.forEach((task, i) => {
       finalTask += `
-      <div class="input-group mb-3">
-        <input type="text" class="form-control" value="${task.task1} - ${task.timestamp}" aria-label="Add a new task" aria-describedby="button-addon2">
-        <button onclick="removeData(${i})" class="btn btn-danger" type="button" id="button2">Remove</button>
-      </div>`;
+      <div class="card custom-card mb-3 card1 col-md-6">
+      <div class="card-body">
+        <input type="text" class="form-control input2" value="${task.task1} - ${task.timestamp}" aria-label="Add a new task" aria-describedby="button-addon2">
+        <button onclick="removeData(${i})" class="btn btn-danger btn-remove mt-3" type="button" id="button2">Remove</button>
+      </div>
+    </div>
+    
+    `;
      });
   main.innerHTML = finalTask;
 };
